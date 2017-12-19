ce1c3a93abcba274aa0e788c0c58aacff8b7d51c
Spencer York <spenceryork@Bernies-MacBook-Pro.local>
2017-12-19 12:33:09 -0600

    connected submit button

--- javascripts/DOMinteraction.js
+++ javascripts/DOMinteraction.js
@@ -1,1 +1,10 @@
 "use strict";
+
+console.log("does this work");
+
+let submit = document.getElementById("submit");
+let output = document.getElementById("output");
+
+submit.addEventListener('click', function(){
+    console.log("button clicked");
+});
