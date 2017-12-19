548dd64d92fb726f35684daf5baeadb7bd1d26b0
Spencer York <spenceryork@Bernies-MacBook-Pro.local>
2017-12-19 13:07:21 -0600

    event listener working

--- javascripts/DOMinteraction.js
+++ javascripts/DOMinteraction.js
@@ -4,7 +4,21 @@ console.log("does this work");
 
 let submit = document.getElementById("submit");
 let output = document.getElementById("output");
+let bread = document.getElementById("bread");
+let meat = document.getElementById("meat");
+let cheese = document.getElementById("cheese");
+let veggies = document.getElementById("veggies");
+let menu = document.getElementById("menu");
 
+//Event listner for button
 submit.addEventListener('click', function(){
     console.log("button clicked");
 });
+
+menu.addEventListener('change', function(){
+    console.log(event.target.value);
+    console.log(event.target.closest('section').id);
+});
+
+
+
