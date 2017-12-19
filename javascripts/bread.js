a5717780e3e58a6e8c5037ad0c05c6b71b857ab3
Spencer York <spenceryork@Bernies-MacBook-Pro.local>
2017-12-19 10:50:05 -0600

    initial setup

---
+++ javascripts/bread.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let breadPrices = {
+    "white": 0.99,
+    "brown": 1.25,
+    "whole wheat": 1.50
+};
+
+const addBread = function(breadSelection) {
+
+};
---
+++ javascripts/meat.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Private variable to store the different meat prices. How is it private?
+let meatPrices = { 
+    salami: 0.75,
+    "roast beef": 1.25,
+    "turkey": 2.00
+}; //why is one key in quotes but the other isn't?
+
+const addMeat = function(meatSelection) {
+  //code that grabs the correct meat price and returns it
+};
+
+module.exports = { addMeat }; //What exactly is being exported here?
